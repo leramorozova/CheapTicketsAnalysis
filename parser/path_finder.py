25ed10d8e7ced25c56eb5bdbffe5bd4eaaac65e2
Alex394540 <alibaba394540@gmail.com>
2021-12-19 16:11:42 +0300

    Path finding algorithm + tests for algorithm

---
+++ parser/path_finder.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import heapq
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import DefaultDict, AnyStr, Tuple
+import pandas as pd
+
+
+@dataclass
+class Path:
+    price: int
+    stages: Tuple[AnyStr]
+
+
+class PathFinder:
+
+    def __init__(self, df_path: str, encoding: str,
+                 origin_field_name='origin_city',
+                 destination_field_name='destination_city',
+                 price_field_name='price'):
+
+        assert os.path.exists(df_path), f"Path '{df_path}' does not exist!"
+        assert encoding, f"Encoding should be set!"
+
+        self.df = pd.read_csv(df_path, encoding=encoding)
+        self.origin_field = origin_field_name
+        self.destination_field = destination_field_name
+        self.price_field = price_field_name
+
+    def get_paths_map(self) -> DefaultDict[AnyStr, DefaultDict[AnyStr, int]]:
+        min_price_df = self.df[
+            [self.origin_field, self.destination_field, self.price_field]]\
+            .groupby([self.origin_field, self.destination_field],
+                     as_index=False).min()
+        paths_map = defaultdict(lambda: defaultdict(lambda: sys.maxsize))
+        for row in min_price_df.itertuples():
+            paths_map[row[1]][row[2]] = row[3]
+        return paths_map
+
+    def find_path(self, v_from: AnyStr, v_to: AnyStr) -> Path:
+        paths_map = self.get_paths_map()
+        visited = set()
+        dists = defaultdict(lambda: sys.maxsize)
+        routes = defaultdict(tuple)
+
+        queue = [(0, v_from)]
+        dists[v_from] = 0
+        routes[v_from] = (v_from,)
+
+        while queue and v_to not in visited:
+            cur_w, cur_v = heapq.heappop(queue)
+            for v, w in paths_map[cur_v].items():
+                if v not in visited and dists[v] > cur_w + w:
+                    heapq.heappush(queue, (cur_w + w, v))
+                    dists[v] = cur_w + w
+                    routes[v] = routes[cur_v] + (v,)
+
+            visited.add(cur_v)
+
+        return Path(dists[v_to], routes[v_to])
---
+++ parser/test_path_finding.py
@@ -0,0 +1,40 @@
+import sys
+from itertools import permutations
+import pytest
+from parser.path_finder import PathFinder
+
+
+@pytest.fixture
+def path_finder_obj():
+    return PathFinder('../data/data3.csv', 'cp1251')
+
+
+@pytest.fixture
+def paths_map(path_finder_obj):
+    return path_finder_obj.get_paths_map()
+
+
+def test_paths_exist(path_finder_obj, paths_map):
+    all_cities = set(path_finder_obj.df[path_finder_obj.origin_field]) \
+                 | set(path_finder_obj.df[path_finder_obj.destination_field])
+    for city_1, city_2 in permutations(all_cities, 2):
+        path = path_finder_obj.find_path(city_1, city_2)
+        computed_price = 0 if path.stages else sys.maxsize
+        prev = city_1
+        for cur_city in path.stages[1:]:
+            computed_price += paths_map[prev][cur_city]
+            prev = cur_city
+        assert computed_price == path.price, \
+            f"""Error while computing path from '{city_1}' to '{city_2}'! 
+              Path: {path}, declared price: {path.price}, 
+              in fact: {computed_price}"""
+
+
+def test_comparison_with_straight_path(path_finder_obj, paths_map):
+    for v, v_paths in paths_map.items():
+        for neigh, direct_flight_price in v_paths.items():
+            path = path_finder_obj.find_path(v, neigh)
+            assert path.price <= direct_flight_price, \
+                f""" Error while computing paths from '{v}' to '{neigh}'!
+                  Straight flight price: {direct_flight_price}, 
+                  minimal cost from algorithm: {path.price}"""
